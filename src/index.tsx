aa947e59d8f7b34d5c4736fb9f0f10cfa7f905f0
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-02-07 15:05:46 +0100

    feat: Tic-tac-toe

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Game from './App';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <Game />
     </React.StrictMode>,
   );
 }
+
