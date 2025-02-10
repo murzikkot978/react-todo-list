2dd1f14294146f2e3ea018f336df16b214cb9792
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-02-10 09:45:14 +0100

    style: add style with classeName attribute
    close #2

--- src/afichage/partie-todo.tsx
+++ src/afichage/partie-todo.tsx
@@ -1,15 +1,37 @@
 function TodoPart() {
   return (
-    <ul className="ulTodoPartie">
-      <li className="liElementTodo">
-        <p>todo1</p>
-      </li>
-      <li className="liElementDateChekDelet">
-        <input type="checkbox" />
-        <p>date1</p>
-        <button className="buttonDelete">delete</button>
-      </li>
-    </ul>
+    <div className="divUlTodoPart">
+      <ul className="ulTodoPartie">
+        <li className="liElementTodo">
+          <p>todo1</p>
+        </li>
+        <li className="liElementDateChekDelet">
+          <input type="checkbox" />
+          <p>date1</p>
+          <button className="buttonDelete">delete</button>
+        </li>
+      </ul>
+      <ul className="ulTodoPartie">
+        <li className="liElementTodo">
+          <p>todo2</p>
+        </li>
+        <li className="liElementDateChekDelet">
+          <input type="checkbox" />
+          <p>date1</p>
+          <button className="buttonDelete">delete</button>
+        </li>
+      </ul>
+      <ul className="ulTodoPartie">
+        <li className="liElementTodo">
+          <p>todo3</p>
+        </li>
+        <li className="liElementDateChekDelet">
+          <input type="checkbox" />
+          <p>date1</p>
+          <button className="buttonDelete">delete</button>
+        </li>
+      </ul>
+    </div>
   );
 }
 
